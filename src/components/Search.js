7ae621c5f841ca22ab8d2acfd8e84e5592296fcd
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-03-15 12:55:09 -0500

    added axios api request to wikipedia

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,15 +2,24 @@ import React, {useState, useEffect} from 'react';
 import axios from "axios";
 
 const Search = () => {
-    const [term, setTerm] = useState('Search for Something ');
+    const [term, setTerm] = useState('');
 
 
+    //axios api call..async and await go inside the function of a useEffect, not part of it
     useEffect(() => {
-        const search = () => {
-
+        const search = async () => {
+            await axios.get('https://en.wikipedia.org/w/api.php', {
+               params: {
+                   action: 'query',
+                   list: 'search',
+                   origin: '*',
+                   format: 'json',
+                   srsearch: term
+               }
+            })
         };
 
-
+        search();
     }, [term]);
 
     const termSubmit = (e) => {
