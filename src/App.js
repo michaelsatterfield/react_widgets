433ab77577ab2ee705436e2095076c76b4d3d28e
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-03-22 19:44:56 -0500

    added dropdown

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 const items = [
     {
@@ -17,11 +18,27 @@ const items = [
     },
 ]
 
+const options =[
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label: 'The Color Green',
+        value: 'green'
+    },
+    {
+        label: 'The Color Blue',
+        value: 'blue'
+    },
+];
+
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () =>{
     return (
         <div>
-           <Search/>
+           {/*<Search/>*/}
+           <Dropdown options={options}/>
         </div>)
 
 }
---
+++ src/components/Dropdown.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Dropdown = ({options}) => {
+
+const renderedOptions = options.map((option) => {
+    return (
+        <div key={option.value } className={"item"}>
+            {option.label}
+        </div>
+    )
+    })
+
+
+    return<h1>{renderedOptions}</h1>
+    ;
+};
+
+export default Dropdown;
